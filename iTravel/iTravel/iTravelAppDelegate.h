4703939baaa48916fc2fe8fa79723c6c597a7cf8
Thainq <thainq@leverages-no-Mac-mini.local>
2013-11-11 17:26:40 +0800

    2

---
+++ iTravel/iTravel/iTravelAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  iTravelAppDelegate.h
+//  iTravel
+//
+//  Created by Thainq on 11/11/2013.
+//  Copyright (c) 2013 Thainq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iTravelAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iTravel/iTravel/iTravelViewController.m
@@ -0,0 +1,29 @@
+//
+//  iTravelViewController.m
+//  iTravel
+//
+//  Created by Thainq on 11/11/2013.
+//  Copyright (c) 2013 Thainq. All rights reserved.
+//
+
+#import "iTravelViewController.h"
+
+@interface iTravelViewController ()
+
+@end
+
+@implementation iTravelViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
