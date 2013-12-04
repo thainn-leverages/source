4cee5c6df3207032f361cef997ea765f7089bfb8
Thainq <thainq@leverages-no-Mac-mini.local>
2013-12-04 09:04:39 +0800

    Revert "17:23"
    This reverts commit 2cba0040c55ddf5c254fccbd0e4b0b3d89a63fd3.
    
    Conflicts:
    
    iTravel/iTravel.xcodeproj/project.xcworkspace/xcuserdata/thainq.xcuserda
    tad/UserInterfaceState.xcuserstate
    	iTravel/iTravel/NewsViewController.m

---
+++ iTravel/iTravel/NewsViewController.m
@@ -0,0 +1,279 @@
+//
+//  NewsViewController.m
+//  iTravel
+//
+//  Created by Thainq on 27/11/2013.
+//  Copyright (c) 2013 Thainq. All rights reserved.
+//
+/*
+ 
+    Becarefull, program can crah when using with MBProgressHUD
+*/
+
+#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+#import "NewsViewController.h"
+#import "NewsCell.h"
+#import "NewsiTravelCell.h"
+#import "DetailNewsiTravelViewController.h"
+
+@interface NewsViewController ()
+@property (nonatomic, strong) NSArray* newsList;
+
+@end
+
+@implementation NewsViewController
+@synthesize segmentnews;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.keysResult = [[NSMutableArray alloc] init];
+
+  //  self.navigationController.title
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
+  
+    [self showLoading];
+   // [self loadData];
+  //  NSLog(@"%d", count);
+}
+
+- (void) loadData
+{
+     
+    str = [NSString stringWithFormat:@"http://192.168.1.224/tourAPI/Tours/getNews"];
+    urlResult =[NSURL URLWithString:str];
+    NSData* data = [NSData dataWithContentsOfURL:urlResult];
+    self.newsList = [NSArray arrayWithArray:[self fetchedDatatoResult:data]];
+ 
+}
+
+-(NSMutableArray *)fetchedDatatoResult: (NSData *)responseData{
+
+    NSMutableArray *news_tmp = [NSMutableArray array];
+    
+    NSError *errorResult = nil;
+    NSString *jsonResult = [NSString stringWithContentsOfURL:urlResult
+                                                    encoding:NSUTF8StringEncoding
+                                                       error:&errorResult];
+    
+    if(!errorResult) {
+        NSData *jsonDataResult = [jsonResult dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary *jsonDictResult = [NSJSONSerialization JSONObjectWithData:jsonDataResult
+                                                                       options:kNilOptions
+                                                                         error:&errorResult];
+        
+         self.keysResult = [jsonDictResult valueForKey:@"News"] ;
+
+         NewsCell *news = [[NewsCell alloc] init];
+         news.name = [self.keysResult valueForKey:@"title"];
+         news.imageData = [self.keysResult valueForKey:@"img"];
+         news.desc = [self.keysResult valueForKey:@"description"];
+        // [news_tmp addObject:news];
+
+        
+        for(int i=0;i<[self.keysResult count];i++){
+        
+            NewsCell *news = [[NewsCell alloc] init];
+            news.name = [[self.keysResult valueForKey:@"title"] objectAtIndex:i];
+            news.imageFile = [[self.keysResult valueForKey:@"img"] objectAtIndex:i];
+            news.desc = [[self.keysResult valueForKey:@"description"] objectAtIndex:i];
+            [news_tmp addObject:news];
+        }
+         
+       [self.tableView reloadData];
+
+     }
+    
+    else{
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Không tải được tin tức!"
+                                                        message:@"Vui lòng thử lại"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Tắt thông báo"
+                                              otherButtonTitles:nil];
+        [alert show];
+        
+    }
+  
+
+   // [HUD removeFromSuperview];
+    return news_tmp;
+
+    
+}
+
+- (void) showLoading{
+  
+    HUD = [[MBProgressHUD alloc] initWithView:self.view];
+    [self.view addSubview:HUD];
+    HUD.mode = MBProgressHUDModeAnnularDeterminate;
+    HUD.delegate = self;
+    HUD.labelText = @"Loading...";
+    [HUD show:YES];
+    
+    [HUD showWhileExecuting:@selector(loadData) onTarget:self withObject:nil animated:YES];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+
+    // Return the number of rows in the section.
+    return [self.keysResult count];
+   
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"NewsCell";
+
+      
+
+    NewsiTravelCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+
+      NewsCell *newss = [self.newsList objectAtIndex:indexPath.row];
+      [cell setDetailsWithNews:newss];
+
+/*
+   // NSLog(@"%@", self.keysResult);
+      UIImageView *langImageView = (UIImageView *)[cell viewWithTag:100];
+    
+  langImageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:[[self.keysResult objectAtIndex:indexPath.row]  valueForKey:@"img"]]]];
+                  
+    UILabel *langLabel = (UILabel *)[cell viewWithTag:101];
+    langLabel.text = [[self.keysResult objectAtIndex:indexPath.row]  valueForKey:@"title"];
+
+    
+    UILabel *langDetailLabel = (UILabel *)[cell viewWithTag:102];
+    langDetailLabel.text =[[self.keysResult objectAtIndex:indexPath.row] valueForKey:@"description"];
+*/    
+    return cell;
+}
+/*
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    [self performSegueWithIdentifier:@"detailnews" sender:self];
+    
+    
+  //   <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+   //  [self.navigationController pushViewController:detailViewController animated:YES];
+    
+}
+*/
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    
+    DetailNewsiTravelViewController *detail = segue.destinationViewController;
+    
+    NSIndexPath* indexPath = [self.tableView indexPathForSelectedRow];
+
+    detail.news = [[self.keysResult valueForKey:@"detail"] objectAtIndex:indexPath.row];
+    
+    
+}
+
+
+- (IBAction)NewsTypeChanged:(UISegmentedControl*)segmentedControl
+{
+    switch (segmentedControl.selectedSegmentIndex)
+    {
+        case 0:
+        //    NSLog(@"0");
+            break;
+        case 1:
+        //    NSLog(@"1");
+            break;
+        case 2:
+        //     NSLog(@"2");
+            break;
+            
+        default:
+            break;
+    }
+ //   NSLog(@"List news");
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+
+
+- (void)viewWillAppear:(BOOL)animated {
+  
+  //  [self.tableView reloadData];
+       
+    
+}
+@end
---
+++ iTravel/iTravel/SearchViewController.m
@@ -0,0 +1,156 @@
+//
+//  SearchViewController.m
+//  iTravel
+//
+//  Created by Thainq on 13/11/2013.
+//  Copyright (c) 2013 Thainq. All rights reserved.
+//
+
+#import "SearchViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    UIColor* mainColor = [UIColor colorWithRed:47.0/255 green:168.0/255 blue:228.0/255 alpha:1.0f];
+  //  UIColor* darkColor = [UIColor colorWithRed:10.0/255 green:78.0/255 blue:108.0/255 alpha:1.0f];
+    
+//    NSString* fontName = @"Avenir-Black";
+    NSString* boldFontName = @"Avenir-Black";
+ //   self.view.backgroundColor = mainColor;
+    
+    self.search.backgroundColor = mainColor;
+    self.search.layer.cornerRadius = 3.0f;
+    self.search.titleLabel.font = [UIFont fontWithName:boldFontName size:20.0f];
+    [self.search setTitle:@"SEARCH" forState:UIControlStateNormal];
+    [self.search setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.search setTitleColor:[UIColor colorWithWhite:1.0f alpha:0.5f] forState:UIControlStateHighlighted];
+    
+    
+    //self.view.backgroundColor = [UI012Color greenColor];
+	// Do any additional setup after loading the view.
+    
+    regionDataFrom = [NSMutableArray arrayWithObjects:@"---All---",@"Tp. Hồ Chí Minh",@"Hà Nội", @"Đà Nẵng", @"Cần Thơ", @"Hải Phòng", @"Nha Trang", @"Huế", nil];
+}
+
+
+- (void) showPicker: (int ) tag{
+    
+    /* first create a UIActionSheet, where you define a title, delegate and a button to close the sheet again */
+    obj_actionSheet_placefrom = [[UIActionSheet alloc]initWithTitle:@"" delegate:self cancelButtonTitle:nil  destructiveButtonTitle:nil otherButtonTitles:nil, nil];
+    
+    [obj_actionSheet_placefrom showInView:self.view.superview];
+    [obj_actionSheet_placefrom setFrame:CGRectMake(0,260,320,300)];
+    [obj_actionSheet_placefrom setTag:tag];
+    UIToolbar *pickerToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    pickerToolbar.barStyle = UIBarStyleBlackOpaque;
+    [pickerToolbar sizeToFit];
+    NSMutableArray *barItems = [[NSMutableArray alloc] init];
+    UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    [barItems addObject:flexSpace];
+    
+    UIBarButtonItem *doneBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(pickerDone)];
+    [barItems addObject:doneBtn];
+    [pickerToolbar setItems:barItems animated:YES];
+    [obj_actionSheet_placefrom addSubview:pickerToolbar];
+ 
+    
+}
+
+- (void)pickerDone{
+
+    [obj_actionSheet_placefrom dismissWithClickedButtonIndex:1 animated:YES];
+    if(!flag){
+        [self.PlaceFrom setTitle:@"---All---" forState:UIControlStateNormal];
+    }
+    else{
+        [self.PlaceFrom setTitle:pFromlbl forState:UIControlStateNormal];
+
+    }
+     
+}
+
+- (IBAction)CallshowPicker:(id)sender {
+   
+    [self showPicker:[sender tag]];
+}
+
+//delegate fucntion for uipicker view
+
+- (void)willPresentActionSheet:(UIActionSheet *)actionSheet{
+    
+    if(actionSheet == obj_actionSheet_placefrom ){
+  
+        pFrom=[[UIPickerView alloc]initWithFrame:CGRectMake(0,40,0,216)];
+        pFrom.showsSelectionIndicator = YES;
+        pFrom.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+        pFrom.delegate = self;
+        pFrom.dataSource = self;
+        [actionSheet addSubview:pFrom];
+        [pFrom setTag:105];
+        [pFrom selectRow:flag inComponent:0 animated:NO];
+    }
+
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component {
+    CGFloat componentWidth = 0.0;
+	componentWidth = self.view.frame.size.width;
+	return componentWidth;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+
+    return [regionDataFrom count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    
+    return [regionDataFrom objectAtIndex:row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    
+    if(pickerView.tag == 105){
+        pFromlbl = [NSString stringWithFormat:@"%@",[regionDataFrom objectAtIndex:[pFrom selectedRowInComponent:0]]];
+        flag = [pFrom selectedRowInComponent:0];
+    }
+
+}
+- (void) showDate{
+    
+    obj_actionSheet_date=[[UIActionSheet alloc]initWithTitle:@"" delegate:self cancelButtonTitle:@"Cancel"  destructiveButtonTitle:@"Done" otherButtonTitles:nil, nil];
+    [obj_actionSheet_date showInView:self.view];
+    [obj_actionSheet_date setFrame:CGRectMake(0,65,320,450)];
+    [obj_actionSheet_date setTag: 101];
+    datepicker.hidden = NO;
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
